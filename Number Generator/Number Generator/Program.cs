02d71f583b3100a50aecaa0c36127ba4e2788d9a
Ahmet Karagöz <61436772+AhmetKaragozz@users.noreply.github.com>
2023-01-26 01:56:47 +0300

    Add files via upload

---
+++ Number Generator/Number Generator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Generator
+{
+    internal class NumberGenerator
+    {
+        static void Main()
+        {
+            Console.Write("Enter the number of random numbers to generate: ");
+            int count = int.Parse(Console.ReadLine());
+
+            var random = new Random();
+            int[] numbers = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = random.Next(1, 100);
+            }
+
+            int countBetween1And99 = numbers.Count(x => x >= 1 && x <= 99);
+            int countBetween1And9 = numbers.Count(x => x >= 1 && x <= 9);
+
+            Console.WriteLine($"Number of numbers between 1 and 99: {countBetween1And99}");
+            Console.WriteLine($"Number of numbers between 1 and 9: {countBetween1And9}");
+        }
+    }
+}
